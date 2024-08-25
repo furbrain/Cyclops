652fd2f2a1514feb4ad6aa797803bcb8474ad50e
Phil Underwood <beardydoc@gmail.com>
2024-08-25 20:14:21 +0100

    Make work via crontab

--- gstreamer/recorder.py
+++ gstreamer/recorder.py
@@ -1,43 +1,62 @@
 #!/usr/bin/env python3
 import board
 import digitalio
-import pwmio
 import time
 import sys
 import subprocess
 import os
+from pathlib import Path
 import signal
 import shutil
+from xmlrpc.client import  ServerProxy
 
-pwm = pwmio.PWMOut(board.D18, frequency=2000)
-pwm.duty_cycle = 0
+HAPPY = (("C6", 0.2), ("E6", 0.2), ("G6", 0.2), ("C7", 0.2))
+SAD = tuple(reversed(HAPPY))
+SHORT = (("C7", 0.1),)
+LONG = (("C7", 0.5),)
+THIS_DIR = Path(__file__).absolute().parent
 
 button = digitalio.DigitalInOut(board.D20)
 button.pull = digitalio.Pull.UP
 vid_count = 0
 
-def wait_for_button_press():
+def wait_for_button_press() -> bool:
     while button.value:
         time.sleep(0.1)
+    start = time.time()
     while not button.value:
         time.sleep(0.1)        
+    return (time.time()-start) > 1.0
 
-def beep(seconds):
-    pwm.duty_cycle = 2 ** 15
-    time.sleep(seconds)
-    pwm.duty_cycle = 0
-    
+
+cmd = [sys.executable, THIS_DIR / "beep_server.py"] 
+beeper_process = subprocess.Popen(cmd, stdout=subprocess.PIPE, 
+                                  shell=False, preexec_fn=os.setsid) 
+time.sleep(1)
+beeper = ServerProxy("http://localhost:8000", allow_none=True)
+
+
+
+beeper.beep(HAPPY)
 while True:
-    wait_for_button_press()
-    beep(0.1)
+    result = wait_for_button_press()
+    if result:
+         print("Shutting down")
+         beeper.beep(SAD)
+         os.killpg(os.getpgid(beeper_process.pid), signal.SIGTERM)
+         subprocess.Popen("sudo shutdown -h now", shell=True)
+         time.sleep(60)
+    beeper.beep(SHORT)
     print("Recording")
-    cmd = "./test.sh"
-    pro = subprocess.Popen(cmd, stdout=subprocess.PIPE, 
-                       shell=True, preexec_fn=os.setsid) 
+    cmd = THIS_DIR / "test.sh"
+    pro = subprocess.Popen(str(cmd), stdout=subprocess.PIPE, 
+                       shell=True, preexec_fn=os.setsid, cwd = str(THIS_DIR)) 
     wait_for_button_press()
     print("Terminating")
     os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
-    beep(0.5)
-    shutil.move("combined.mkv", f"vid{vid_count}.mkv")
+    beeper.beep(LONG)
+    time.sleep(1)
+    shutil.move("/home/pi/combined.mkv", f"/home/pi/vid{vid_count}.mkv")
     vid_count += 1
     print("Finished recording")
+
