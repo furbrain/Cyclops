9768034ddfdde48826caa1289a60c94183459ebc
Phil Underwood <beardydoc@gmail.com>
2024-09-28 11:17:01 +0100

    Work on cmaera registration  and glitch detection process

--- TOF/glitch_detector.py
+++ TOF/glitch_detector.py
@@ -1,13 +1,13 @@
 from reader.gstreader import VidReader, TOFReader, IMUReader
 
-filename = "/home/phil/tmp2.mkv"
+filename = "/home/phil/cal.mkv"
 
 READERS = {
-    VidReader: f"""filesrc location="{filename}" ! matroskademux ! video/x-h264 ! decodebin ! 
+    VidReader: f"""filesrc location="{filename}" ! matroskademux ! video/x-h264 ! decodebin !
                             videoconvert ! video/x-raw,format=GRAY8 ! appsink sync=false""",
     TOFReader: f'''filesrc location="{filename}" ! matroskademux !
           video/x-raw,width=480,format=GRAY8 ! appsink sync=false''',
-    IMUReader: f"""filesrc location="{filename}" name=fsrc ! matroskademux name=demux !  
+    IMUReader: f"""filesrc location="{filename}" name=fsrc ! matroskademux name=demux !
                             video/x-raw ! appsink name=telemetry sync=false"""
 }
 
@@ -22,3 +22,4 @@ for cls, spec in READERS.items():
             if tm - last_tm > 0.1:
                 print(f"    Glitch at: {tm}; {tm - last_tm}")
             last_tm = tm
+print("done")
