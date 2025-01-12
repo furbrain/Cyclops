9cad9a8123e63e2d3bbd85230e82626e1f12522c
Phil Underwood <beardydoc@gmail.com>
2025-01-12 16:43:17 +0000

    make more modular camera finder

--- gstreamer/find_cameras2.py
+++ gstreamer/find_cameras2.py
@@ -7,18 +7,15 @@ def find_x_cams(x: str) -> Set[int]:
     devs = pth.glob("**/*usb*/**/video?")
     return set(int(str(x)[-1]) for x in devs)
 
-def find_usb_cams() -> Set[int]:
-    return find_x_cams("usb")
-
-def find_csi_cams() -> Set[int]:
-    return find_x_cams("csi")
-
 def matches_format(dev:int, fmt:bytes) -> bool:
     output: bytes = subprocess.check_output(["v4l2-ctl", "-d", str(dev), "--list-formats"])
     return fmt in output
 
-def get_matching_formats(nums: Set[int], fmt:bytes) -> List[int]:
-    return [int(x) for x in nums if matches_format(x, fmt)]
+def get_matching_formats(cam_type: str, fmt:str = None) -> List[int]:
+    cams = find_x_cams(cam_type)
+    if fmt is None:
+        return list(cams)
+    return [int(x) for x in cams if matches_format(x, bytes(fmt,'UTF8'))]
 
 
 if __name__=="__main__":
