0abbf0cf701e3230bc094865667e850345674b54
Phil Underwood <beardydoc@gmail.com>
2025-03-17 00:46:39 +0000

    Get depth filling code working

---
+++ ROS/src/cyclops_launch/scripts/fill_depth_image.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+import numpy as np
+from sensor_msgs.msg import CameraInfo, Image
+import message_filters
+import rospy
+from cv_bridge import CvBridge
+import cv2
+
+class FillDepthNode:
+    def __init__(self):
+        self.sub = rospy.Subscriber("in", Image, self.callback, queue_size=5)
+        self.pub = rospy.Publisher("out", Image, queue_size=5)
+        self.bridge = CvBridge()
+        self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+
+
+    def callback(self, img_msg: Image):
+        img = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding='passthrough')
+        img = img.copy()
+        img[np.isnan(img)] = 0
+        img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, self.kernel)
+        header = img_msg.header
+        msg = self.bridge.cv2_to_imgmsg(img.astype(np.float32), encoding='32FC1')
+        msg.header = header
+        self.pub.publish(msg)
+        
+if __name__=="__main__":
+    rospy.init_node("radial_node")
+    FillDepthNode()
+    rospy.spin()
