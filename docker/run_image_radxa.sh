0d954d9f30338a82c1e810133250d41768effe15
Phil <beardydoc@gmail.com>
2025-05-29 12:25:15 +0000

    docker updates

--- docker/run_image_radxa.sh
+++ docker/run_image_radxa.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Starting docker instance"
-docker run --network host --privileged --ipc="host" -v /home/radxa:/home/cyclops -v /home/radxa/bags:/bags -v /var/run/dbus:/var/run/dbus -v /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket test:latest
+docker run --network host --privileged --ipc="host" -v /:/host_fs/ -v /home/radxa:/home/cyclops -v /home/radxa/bags:/bags -v /var/run/dbus:/var/run/dbus -v /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket test:latest
