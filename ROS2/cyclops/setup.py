1e9a763016a8a2d66786d9e8eb85cc0144b3b089
Phil Underwood <beardydoc@gmail.com>
2025-09-07 20:53:40 +0100

    update setup.py

--- ROS2/cyclops/setup.py
+++ ROS2/cyclops/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'cyclops'
@@ -7,9 +9,9 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +22,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+		    "svc = cyclops.simple_service:main",
+            "calibrate = cyclops.calibrate:main",
+            "cameras = cyclops.cameras:main",
         ],
     },
 )
