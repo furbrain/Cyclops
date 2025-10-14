840648d938b267c67081acdccf2c20439cecb3e7
Phil Underwood <beardydoc@gmail.com>
2025-10-14 22:02:38 +0100

    Make ros3djs viewers dynamic

--- www/js/cyclops.js
+++ www/js/cyclops.js
@@ -44,7 +44,7 @@ function reset_button(btn) {
 
 
 //connect all .ros-service buttons to Trigger services
-const btns_service = document.querySelectorAll('button.ros_service');
+const btns_service = document.querySelectorAll('button.ros-service');
 btns_service.forEach(function(btn) {
     btn.old_text = btn.innerText;
     btn.old_classes = btn.className;
@@ -174,36 +174,72 @@ cal_btns.forEach(function(btn) {
     };
 });
 
-
-// setup 3d viewer
-// Create the main viewer.
-var viewer = new ROS3D.Viewer({
-  divID : 'viewer',
-  width : 800,
-  height : 600,
-  antialias : true
-});
-
 // Setup a client to listen to TFs.
 var tfClient = new ROSLIB.ROS2TFClient({
   ros : ros,
   angularThres : 0.01,
   transThres : 0.01,
   rate : 10.0,
-  fixedFrame : '/map'
+  fixedFrame : '/imu'
 });
 
-var cloudClient = new ROS3D.PointCloud2({
-      ros: ros,
-      tfClient: tfClient,
-      rootObject: viewer.scene,
-      topic: '/imu/cal/all_scaled_mag_points',
-      material: { size: 1.0, color: 0xff00ff }
+function addPointClouds(viewer) {
+    const topics = viewer.dataset.pointclouds.split(/\s+/).filter(t => t);
+    viewer.pointClouds = []
+    topics.forEach(function(topic) {
+        viewer.pointClouds.push(new ROS3D.PointCloud2({
+          ros: ros,
+          tfClient: tfClient,
+          rootObject: viewer.viewer.scene,
+          topic: topic,
+          material: { size: 1.0, color: 0xff00ff }
+        }));
     });
+}
 
-var markerClient = new ROS3D.MarkerClient({
-    ros: ros,
-    tfClient: tfClient,
-    rootObject: viewer.scene,
-    topic: "/imu/cal/marker",
+function addMarkers(viewer) {
+    const topics = viewer.dataset.markers.split(/\s+/).filter(t => t);
+    viewer.markers = []
+    topics.forEach(function(topic) {
+        viewer.markers.push(new ROS3D.MarkerClient({
+          ros: ros,
+          tfClient: tfClient,
+          rootObject: viewer.viewer.scene,
+          topic: topic
+        }));
+    });
+}
+
+const viewer_observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.debug('Viewer is now visible: ' + entry.target.id);
+      entry.target.viewer.start();
+    } else {
+      console.debug('Viewer is no longer visible: ' + entry.target.id);
+      entry.target.viewer.stop();
+    }
+  });
 });
+
+
+const viewers = document.querySelectorAll('div.ros-viewer');
+viewers.forEach(function(viewer_div) {
+    console.log("Processing viewer: "+ viewer_div.id)
+    scale = viewer_div.dataset.scale || 10;
+    viewer_div.viewer = new ROS3D.Viewer({
+      divID : viewer_div.id,
+      width : 800,
+      height : 600,
+      antialias : true,
+      cameraZoomSpeed : 1.0,
+      cameraPose: {x: scale, y: scale, z: scale}
+    });
+    viewer_div.grid = new ROS3D.Grid();
+    viewer_div.viewer.addObject(viewer_div.grid);
+    addPointClouds(viewer_div);
+    addMarkers(viewer_div);
+    viewer_observer.observe(viewer_div);
+});
+
+
