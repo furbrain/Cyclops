8d3a786c05e05df390057c1809b2a23dbd9a8f38
Phil Underwood <beardydoc@gmail.com>
2026-06-02 23:07:56 +0100

    Add slideshow to web API

--- raspbian/start_gunicorn.sh
+++ raspbian/start_gunicorn.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 source /home/pi/ros2_ws/install/setup.bash
-exec /usr/bin/gunicorn -b 127.0.0.1:5000 app:app --reload
+exec /usr/bin/gunicorn -b 127.0.0.1:5000 app:app --reload --worker-class gevent
