5a5489c52501b9c12616b0d8a7d22403142c1b98
Phil Underwood <beardydoc@gmail.com>
2026-06-12 20:54:00 +0100

    More robust (and simpler) camera launch

--- ROS2/cyclops/launch/binoc.py
+++ ROS2/cyclops/launch/binoc.py
@@ -6,7 +6,8 @@ import os
 
 tuning_file = os.path.join(get_package_share_directory('cyclops'), 'config/imx462.json')
 STAGGER_DELAY = 8.0  # seconds to wait before starting second camera
-
+RESPAWN_DELAY = 2.0 
+MAX_RESTARTS=5
 IPA_ENV = {'LIBCAMERA_RPI_TUNING_FILE': tuning_file}
 PARAMS = {
     'SyncFrames': 10,   # should match between client and server
@@ -22,43 +23,77 @@ PARAMS = {
 
 REMAPPINGS = [('~/'+x,x) for x in ('image_raw', 'image_raw/compressed', 'camera_info', 'set_camera_info')]
 
-def generate_launch_description():
-    left_camera = Node(
+# def generate_launch_description():
+#     left_camera = Node(
+#         package='camera_ros',
+#         executable='camera_node',
+#         name='camera',
+#         namespace='left',
+#         parameters=[{
+#             'camera': "/base/axi/pcie@1000120000/rp1/i2c@70000/arducam_pivariety@c",
+#             'SyncMode': 2,      # SyncModeClient (should be started first)
+#             'frame_id': 'left',
+#             'camera_info_url': 'file://${ROS_HOME}/camera_info/left.yaml'
+#         }, PARAMS],
+#         remappings=REMAPPINGS,
+#         additional_env=IPA_ENV,
+#     )
+#
+#     right_camera = TimerAction(
+#         period=STAGGER_DELAY,
+#         actions=[
+#             Node(
+#                 package='camera_ros',
+#                 executable='camera_node',
+#                 name='camera',
+#                 namespace='right',
+#                 parameters=[{
+#                     'camera': "/base/axi/pcie@1000120000/rp1/i2c@88000/arducam_pivariety@c",
+#                     'SyncMode': 1,      # SyncModeServer
+#                     'frame_id': 'right',
+#                     'camera_info_url': 'file://${ROS_HOME}/camera_info/right.yaml'
+#                 }, PARAMS],
+#                 remappings=REMAPPINGS,
+#                 additional_env=IPA_ENV,
+#             )
+#         ]
+#     )
+#
+#     return LaunchDescription([
+#         left_camera,
+#         right_camera,
+#     ])
+
+
+# Per-camera config: namespace, i2c path, SyncMode (2=client/left, 1=server/right)
+CAMERAS = [
+    ('left', '/base/axi/pcie@1000120000/rp1/i2c@70000/arducam_pivariety@c', 2),
+    ('right', '/base/axi/pcie@1000120000/rp1/i2c@88000/arducam_pivariety@c', 1),
+]
+
+
+def make_camera_node(namespace, camera_path, sync_mode):
+    return Node(
         package='camera_ros',
         executable='camera_node',
         name='camera',
-        namespace='left',
+        namespace=namespace,
         parameters=[{
-            'camera': "/base/axi/pcie@1000120000/rp1/i2c@70000/arducam_pivariety@c",
-            'SyncMode': 2,      # SyncModeClient (should be started first)
-            'frame_id': 'left',
-            'camera_info_url': 'file://${ROS_HOME}/camera_info/left.yaml'
+            'camera': camera_path,
+            'SyncMode': sync_mode,
+            'frame_id': namespace,
+            'camera_info_url': f'file://${{ROS_HOME}}/camera_info/{namespace}.yaml',
         }, PARAMS],
         remappings=REMAPPINGS,
         additional_env=IPA_ENV,
+        respawn=True,
+        respawn_max_retries=MAX_RESTARTS,
+        respawn_delay=RESPAWN_DELAY
     )
 
-    right_camera = TimerAction(
-        period=STAGGER_DELAY,
-        actions=[
-            Node(
-                package='camera_ros',
-                executable='camera_node',
-                name='camera',
-                namespace='right',
-                parameters=[{
-                    'camera': "/base/axi/pcie@1000120000/rp1/i2c@88000/arducam_pivariety@c",
-                    'SyncMode': 1,      # SyncModeServer
-                    'frame_id': 'right',
-                    'camera_info_url': 'file://${ROS_HOME}/camera_info/right.yaml'
-                }, PARAMS],
-                remappings=REMAPPINGS,
-                additional_env=IPA_ENV,
-            )
-        ]
-    )
 
+def generate_launch_description():
     return LaunchDescription([
-        left_camera,
-        right_camera,
+        make_camera_node(namespace, camera_path, sync_mode)
+        for namespace, camera_path, sync_mode in CAMERAS
     ])
